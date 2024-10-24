cd9d06802a932314302cd75d506e62f1399021bf
Archange <archange.tshikayakalala@monboreal.ca>
2024-10-24 13:49:01 -0400

    Ajout de la classe mère Paiement

---
+++ AppPaiements/AppPaiements/Paiement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paiement
+    {
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+        public Paiement(double montant, string description)
+        {
+            Montant = montant;
+            Description = description;
+        }
+        public void AfficherDetails() 
+        { 
+            Console.WriteLine($"Paiement de {Montant}$ - {Description}"); 
+        }
+    }   
+   
+
+
+}
+
