f611df2321bd6f43ff202d545aed2bedfa5d7457
Archange <archange.tshikayakalala@monboreal.ca>
2024-10-24 14:39:34 -0400

    Ajout de la 2e classe fille Paypal

---
+++ AppPaiements/AppPaiements/PayPal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class PayPal : Paiement
+    {
+        public string Courriel {  get; set; }
+        public PayPal(double montant, string description, string courriel)
+        : base(montant, description)
+        {
+            Courriel = courriel;
+        }
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Paiement par Paypal : {Montant}$ - {Description} - Courriel : {Courriel}");
+        }
+    }
+}
